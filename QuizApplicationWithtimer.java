d38300958674b94f260c65750e6779740b004c23
aliMobasshir <mobasshir0001@gmail.com>
2024-08-15 20:33:22 +0530

    bugs fixed

--- QuizApplicationWithtimer.java
+++ QuizApplicationWithtimer.java
@@ -4,7 +4,7 @@ import java.util.TimerTask;
 
 public class QuizApplicationWithtimer {
   public static void main(String[] args) {
-
+Scanner sc = new Scanner(System.in);
 int score =0;
 
     String[] questions={"What is the capital of Norway ?","Which Mughal emperor was famous as Zinda Pir ?","What is the only even prime number?"};
@@ -16,8 +16,18 @@ int score =0;
     for(int currentQuestion=0;currentQuestion<questions.length;currentQuestion++){
         System.out.println(questions[currentQuestion]);
 
-        for(int j=0;j<options[currentQuestion].length;j++){
+          for(int j=0;j<options[currentQuestion].length;j++){
           System.out.println(options[currentQuestion][j]);
+        }
+        
+        System.out.println("Enter your answer A/B/C/D:");
+        char userAnswer = sc.next().charAt(0);
+
+        if (userAnswer == answers[currentQuestion]) {
+            System.out.println("Correct!");
+            score++;
+        } else {
+            System.out.println("Incorrect. The correct answer was option " + answers[currentQuestion] + ".");
         }
       }
 
